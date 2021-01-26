3cb80e879894c879ace6d59d1052266a16bb98fe
Goalof <68434460+Goalof@users.noreply.github.com>
2021-01-26 19:52:56 +0600

    Changes

--- src/components/EmbedHTML.js
+++ src/components/EmbedHTML.js
@@ -14,28 +14,28 @@ const customHtml = `
 }
 @font-face {
   font-family: 'AvenirNextCyrRegular';
-  src: url('https://ukit.top/fonts/AvenirNextCyr-Regular.eot');
-  src: url('https://ukit.top/fonts/AvenirNextCyr-Regular.eot?#iefix') format('embedded-opentype'), url('https://ukit.top/fonts/AvenirNextCyr-Regular.woff') format('woff'), url('https://ukit.top/fonts/AvenirNextCyr-Regular.ttf') format('truetype');
+  src: url('https://github.com/Goalof/ruteam/blob/main/static/fonts/AvenirNextCyr-Regular.eot');
+  src: url('https://github.com/Goalof/ruteam/blob/main/static/fonts/AvenirNextCyr-Regular.eot?#iefix') format('embedded-opentype'), url('https://github.com/Goalof/ruteam/blob/main/static/fonts/AvenirNextCyr-Regular.woff') format('woff'), url('https://github.com/Goalof/ruteam/blob/main/static/fonts/AvenirNextCyr-Regular.ttf') format('truetype');
   font-weight: normal;
   font-style: normal;
 }
 @font-face {
   font-family: 'AvenirNextCyrMedium';
-  src: url('https://ukit.top/fonts/AvenirNextCyr-Medium.eot');
-  src: url('https://ukit.top/fonts/AvenirNextCyr-Medium.eot?#iefix') format('embedded-opentype'), url('https://ukit.top/fonts/AvenirNextCyr-Medium.woff') format('woff'), url('https://ukit.top/fonts/AvenirNextCyr-Medium.ttf') format('truetype');
+  src: url('https://github.com/Goalof/ruteam/blob/main/static/fonts/AvenirNextCyr-Medium.eot');
+  src: url('https://github.com/Goalof/ruteam/blob/main/static/fonts/AvenirNextCyr-Medium.eot?#iefix') format('embedded-opentype'), url('https://github.com/Goalof/ruteam/blob/main/static/fonts/AvenirNextCyr-Medium.woff') format('woff'), url('https://github.com/Goalof/ruteam/blob/main/static/fonts/AvenirNextCyr-Medium.ttf') format('truetype');
   font-weight: normal;
   font-style: normal;
 }
 @font-face {
   font-family: 'AvenirNextCyrBold';
-  src: url('https://ukit.top/fonts/AvenirNextCyr-Bold.eot');
-  src: url('https://ukit.top/fonts/AvenirNextCyr-Bold.eot?#iefix') format('embedded-opentype'), url('https://ukit.top/fonts/AvenirNextCyr-Bold.woff') format('woff'), url('https://ukit.top/fonts/AvenirNextCyr-Bold.ttf') format('truetype');
+  src: url('https://github.com/Goalof/ruteam/blob/main/static/fonts/AvenirNextCyr-Bold.eot');
+  src: url('https://github.com/Goalof/ruteam/blob/main/static/fonts/AvenirNextCyr-Bold.eot?#iefix') format('embedded-opentype'), url('https://github.com/Goalof/ruteam/blob/main/static/fonts/AvenirNextCyr-Bold.woff') format('woff'), url('https://github.com/Goalof/ruteam/blob/main/static/fonts/AvenirNextCyr-Bold.ttf') format('truetype');
   font-weight: normal;
   font-style: normal;
 }
 @font-face {
   font-family: 'AvenirNextCyrDemi';
-  src: url('https://ukit.top/fonts/AvenirNextCyr-Demi.otf');
+  src: url('https://github.com/Goalof/ruteam/blob/main/static/fonts/AvenirNextCyr-Demi.otf');
   font-weight: normal;
   font-style: normal;
 }
